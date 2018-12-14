364c481d07894dc98c8f5994ab134be76aea3d36
Dake He <dhe@us.costar.local>
2018-12-14 12:35:56 -0800

    init check-in

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Adds commas to a number
+ * @param {number} number
+ * @param {string} locale
+ * @return {string}
+ */
+module.exports.test = function(value) {
+    return `my first module : ${value}`;
+};
